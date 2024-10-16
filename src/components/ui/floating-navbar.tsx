c9b7f757d00af6405bf9933c494bd7716ebad5f1
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-10-16 16:23:32 +0000

    Update floating-navbar.tsx

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -73,7 +73,7 @@ export const FloatingNav = ({
           </Link>
         ))}
         <button className="border text-sm font-medium relative border-neutral-200/30 dark:border-white/[0.2] text-white dark:text-white px-4 py-2 rounded-full">
-          <span>Login</span>
+          <span>Join Us</span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
         </button>
       </motion.div>
