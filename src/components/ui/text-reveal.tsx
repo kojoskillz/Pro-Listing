18643812f1eb8ad0e3444a394e9f6d8388d2c357
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-10-16 16:25:47 +0000

    Update text-reveal.tsx

--- src/components/ui/text-reveal.tsx
+++ src/components/ui/text-reveal.tsx
@@ -31,7 +31,7 @@ export const TextRevealByWord: FC<TextRevealByWordProps> = ({
         <p
           ref={targetRef}
           className={
-            "flex flex-wrap text-center p-5 text-2xl font-bold text-white dark:text-white md:p-8 md:text-3xl lg:p-10 lg:text-4xl xl:text-5xl"
+            "flex flex-wrap text-center p-5 text-4xl font-bold text-white dark:text-white md:p-8 md:text-3xl lg:p-10 lg:text-4xl xl:text-5xl"
           }
         >
           {words.map((word, i) => {
