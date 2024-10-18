db2399360942ec99f98ffaac41b2838b07329390
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-10-18 18:43:57 +0000

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import ShineBorder from "../components/ui/shine-border";
 import img7 from "../app/assets/img7.png";
 import img2 from "../app/assets/img2.png";
 import img3 from "../app/assets/img3.png";
-import img5 from "../app/assets/img5.png";
+import imgX from "../app/assets/img1.png";
 import img6 from "../app/assets/img6.png";
 import Image from "next/image";
 
@@ -13,7 +13,7 @@ export default function ShineBorderDemo() {
 <div className="mb-20">
 
     <div className="grid ">
-    <h1 className="text-white agdasima text-5xl -mt-[23rem] lg:-mt-[7rem]  font-bold text-center">
+    <h1 className="text-white agdasima text-5xl leading-8 -mt-[23rem] lg:-mt-[7rem]  font-bold text-center">
       Our Members <span className="text-blue-500"> Accomplishment</span> 
     </h1>
   </div>
@@ -26,7 +26,10 @@ export default function ShineBorderDemo() {
       color={["#74747439", "#7272724A", "#B8B8B84B"]}
     >
 
-  <div className="bg-white w-44 rounded-full p-1 ml-auto">
+  <div className="bg-white flex lg:w-44 w-48 rounded-full p-1 ml-auto">
+               <h1 className="text-bold text-nowrap m-auto ml-2 tracking-tighter leading-4 text-left text-slate-600 font-bold text-[16.8px] lg:text-[15px] mr-auto">
+                    Successful Tokens
+               </h1> 
             <Image 
                   src={img7}
                   alt="star_image"
@@ -35,15 +38,14 @@ export default function ShineBorderDemo() {
                   sizes="fit-content"
                   className="img0"
               />  
+               
   </div>
 
 
       <span className="pointer-events-none mr-auto  whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text agdasima text-[7rem] lg:text-[9rem] font-semibold leading-none dark:from-white dark:to-slate-100/10">
         100+
       </span>
-      <h1 className="text-bold text-left  text-blue-500 text-lg mr-auto">
-          Successful Tokens
-      </h1>
+            
       <p className="text-gray-200/70 text-[18px] ">
         Over 100 tokens listed on CoinMarketCap,CoinGecko, and leading centralized exchanges.
       </p>
@@ -73,7 +75,7 @@ export default function ShineBorderDemo() {
           Client Satisfaction
       </h1>
       <p className="text-gray-200/70 text-[18px] ">
-         Consistentlly delivering high-quality service with a 95% client satisfaction
+         Consistently delivering high-quality service with a 95% client satisfaction
       </p>
     </ShineBorder>
 
@@ -82,22 +84,22 @@ export default function ShineBorderDemo() {
       color={["#74747439", "#7272724A", "#B8B8B84B"]}
     >
       
-  <div className="bg-white w-32 h-[3rem] rounded-full p-1 ml-auto">
+  <div className="bg-white flex text-wrap text-center w-[13.7rem] h-[3rem] rounded-full p-1 ml-auto">
             <Image 
                   src={img3}
                   alt="star_image"
-                  width="50"
-                  height="40"
+                  width="60"
+                  height="60"
                   sizes="fit-content"
                   className="img3"
               /> 
+               <h1 className="text-bold text-center m-auto tracking-tighter leading-4  text-slate-600 font-bold text-[16.8px] lg:text-[15px] mr-auto">
+                  Marketing Campaigns Executed
+               </h1>
    </div>
       <span className="pointer-events-none mr-auto whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text  agdasima text-[7rem] lg:text-[9rem] font-semibold leading-none dark:from-white dark:to-slate-100/10">
         50+
       </span>
-      <h1 className="text-bold  text-blue-500 text-lg mr-auto">
-         Marketing Campaigns Executed
-      </h1>
       <p className="text-gray-200/70 text-[18px] ">
         Launched and managed over 50 successful marketing campaigns, driving significant engagement and growth.
       </p>
@@ -108,22 +110,23 @@ export default function ShineBorderDemo() {
       color={["#74747439", "#7272724A", "#B8B8B84B"]}
     >
 
-<div className="bg-white w-44 h-[3.5rem] rounded-full p-1 ml-auto">
+<div className="bg-white flex text-nowrap w-44 h-[3.5rem] rounded-full p-1 ml-auto">
               <Image 
-                  src={img5}
+                  src={imgX}
                   alt="star_image"
-                  width="50"
+                  width="60"
                   height="60"
                   sizes="fit-content"
-                  className="img0"
+                  className="imgX"
               /> 
+           <h1 className="text-bold m-auto -ml-2 text-left text-slate-600 font-bold text-[16.8px] lg:text-[15px] mr-auto">
+              Faster Listings
+           </h1>
 </div>
       <span className="pointer-events-none  mr-auto whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text  agdasima text-[7rem] lg:text-[9rem] font-semibold leading-none dark:from-white dark:to-slate-100/10">
         80%
       </span>
-      <h1 className="text-bold text-left text-blue-500 text-lg mr-auto">
-        Faster Listings
-      </h1>
+    
       <p className="text-gray-200/70 text-[18px] ">
         Achieved lisitngs up to 80% faster than industry averages, ensuring your tokens gets to market quickly.
       </p>
