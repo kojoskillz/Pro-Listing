08eeb3189ee07b16d5082ae21444c396e0115463
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-10-19 12:55:05 +0000

    Update Accordion.tsx

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -7,57 +7,68 @@ import {
   
   export default function AccordionDemo() {
     return (
+
+      
       <Accordion type="single" collapsible className="mb-20 w-[22rem] md:w-[50rem] lg:w-[60rem] m-auto ">
+         <div>
+            <h1 className="text-white agdasima text-center text-[45px] lg:text-[90px]">
+                What Our Clients Say
+            </h1>
+            <p className="text-gray-300  text-center text-[20px] lg:text-[30px] mb-10 ">
+                Don't just take our word for it...
+                Our clients' feedback is essential to us. Here's what they have to say
+            </p>
+        </div>
         <AccordionItem value="item-1">
-          <AccordionTrigger className="text-[16px] lg:text-4xl md:text-[20px]  ">What services does your agency provide?</AccordionTrigger>
+          <AccordionTrigger className="text-[16px] lg:text-3xl md:text-[18.5px]  ">What services does your agency provide?</AccordionTrigger>
           <AccordionContent className="text-[11.5px] lg:text-[17px] md:text-[14px] font-normal text-wrap text-gray-300  ">
           We offer a range of services including token listings on CoinMarketCap and CoinGecko, marketing, shilling, trending, community management, and growth strategies.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
-          <AccordionTrigger className="text-[16px] lg:text-4xl md:text-[20px]  ">How long does the token listing process take?</AccordionTrigger>
+          <AccordionTrigger className="text-[16px] lg:text-3xl md:text-[18.5px]  ">How long does the token listing process take?</AccordionTrigger>
           <AccordionContent className="text-[11.5px] lg:text-[17px] md:text-[14px] font-normal text-wrap text-gray-300  ">
               We expedite listings by up to 80%.. within 24 - 72hrs.  The exact timeframe depends on the platform and specific requirements of your token.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3">
-          <AccordionTrigger className="text-[16px] lg:text-4xl md:text-[20px]  ">Do you list tokens with low volume?</AccordionTrigger>
+          <AccordionTrigger className="text-[16px] lg:text-3xl md:text-[18.5px]  ">Do you list tokens with low volume?</AccordionTrigger>
           <AccordionContent className="text-[11.5px] lg:text-[17px] md:text-[14px] font-normal text-wrap text-gray-300  ">
           Yes, we do list tokens with low volume. Our focus is on helping projects of all sizes gain visibility and traction. We work with you to develop strategies that can enhance your token’s market presence and drive growth.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-4">
-          <AccordionTrigger className="text-[16px] lg:text-4xl md:text-[20px]  "> What are your pricing structures?</AccordionTrigger>
+          <AccordionTrigger className="text-[16px] lg:text-3xl md:text-[18.5px]  "> What are your pricing structures?</AccordionTrigger>
           <AccordionContent className="text-[11.5px] lg:text-[17px] md:text-[14px] font-normal text-wrap text-gray-300  ">
           Our pricing is transparent and varies based on the services you choose. We offer detailed quotes tailored to your project’s needs, with no hidden fees.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-5">
-          <AccordionTrigger className="text-[16px] lg:text-4xl md:text-[20px]  "> Will I receive a detailed breakdown of costs?</AccordionTrigger>
+          <AccordionTrigger className="text-[16px] lg:text-3xl md:text-[18.5px]  "> Will I receive a detailed breakdown of costs?</AccordionTrigger>
           <AccordionContent className="text-[11.5px] lg:text-[17px] md:text-[14px] font-normal text-wrap text-gray-300  ">
           Yes, we provide detailed quotes tailored to your project's unique needs.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-6">
-          <AccordionTrigger className="text-[16px] lg:text-4xl md:text-[20px]  ">Do you list tokens with low volume??</AccordionTrigger>
+          <AccordionTrigger className="text-[16px] lg:text-3xl md:text-[18.5px]  ">Do you list tokens with low volume??</AccordionTrigger>
           <AccordionContent className="text-[11.5px] lg:text-[17px] md:text-[14px] font-normal text-wrap text-gray-300  ">
           Yes, we do list tokens with low volume. Our focus is on helping projects of all sizes gain visibility and traction. We work with you to develop strategies that can enhance your token’s market presence and drive growth.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-7">
-          <AccordionTrigger className="text-[16px] lg:text-4xl md:text-[20px]  ">Are there any hidden fees?</AccordionTrigger>
+          <AccordionTrigger className="text-[16px] lg:text-3xl md:text-[18.5px]  ">Are there any hidden fees?</AccordionTrigger>
           <AccordionContent className="text-[11.5px] lg:text-[17px] md:text-[14px] font-normal text-wrap text-gray-300  ">
           No, we pride ourselves on transparent pricing with no additional hidden fees.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-8">
-          <AccordionTrigger className="text-[16px] lg:text-4xl md:text-[20px]  "> What factors influence the cost of my project?</AccordionTrigger>
+          <AccordionTrigger className="text-[16px] lg:text-3xl md:text-[18.5px]  "> What factors influence the cost of my project?</AccordionTrigger>
           <AccordionContent className="text-[11.5px] lg:text-[17px] md:text-[14px] font-normal text-wrap text-gray-300  ">
           The cost will depend on the specific services, scope, complexity, and duration of your project.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-9">
-          <AccordionTrigger className="text-[16px] lg:text-4xl md:text-[20px]  ">Do you offer package deals or discounts?</AccordionTrigger>
+          <AccordionTrigger className="text-[16px] lg:text-3xl md:text-[18.5px]  ">Do you offer package deals or discounts?</AccordionTrigger>
           <AccordionContent className="text-[11.5px] lg:text-[17px] md:text-[14px] font-normal text-wrap text-gray-300  ">
           Contact us to inquire about potential promotions, packages, or discounts that may apply to your project.
           </AccordionContent>
